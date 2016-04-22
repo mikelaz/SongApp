b290e81dac4be2c384fdf75cb061cda54250e752
Mikel Aguirre <mikelk35@gmail.com>
2016-04-22 09:09:46 +0200

    Initial Commit

---
+++ SongApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SongApp
+//
+//  Created by Mikel Aguirre on 22/4/16.
+//  Copyright © 2016 Mikel Aguirre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
